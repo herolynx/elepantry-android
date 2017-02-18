ac0d1a6142230dae18420524f24ffd87f992d34e
wrona <michal.robert.wronski@gmail.com>
2017-02-18 18:02:05 +0100

    [#32] Left menu moved to separate class.

--- app/src/main/java/com/herolynx/elepantry/resources/ResourcesActivity.kt
+++ app/src/main/java/com/herolynx/elepantry/resources/ResourcesActivity.kt
@@ -1,28 +1,15 @@
 package com.herolynx.elepantry.resources
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.NavigationView
-import android.support.design.widget.Snackbar
-import android.support.v4.view.GravityCompat
-import android.support.v4.widget.DrawerLayout
-import android.support.v7.app.ActionBarDrawerToggle
-import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.support.v7.widget.Toolbar
-import android.view.Menu
-import android.view.MenuItem
-import android.view.ViewGroup
 import com.herolynx.elepantry.R
 import com.herolynx.elepantry.core.log.debug
 import com.herolynx.elepantry.core.ui.recyclerview.ListAdapter
 import com.herolynx.elepantry.core.ui.recyclerview.onInfiniteLoading
 import com.herolynx.elepantry.ext.google.drive.GoogleDrive
 import com.herolynx.elepantry.ext.google.drive.GoogleDriveSearch
-import com.herolynx.elepantry.ext.google.firebase.db.FirebaseDb
-import com.herolynx.elepantry.getAppContext
-import com.herolynx.elepantry.menu.UserBadge
+import com.herolynx.elepantry.menu.LeftMenu
 import com.herolynx.elepantry.resources.model.Resource
 import com.herolynx.elepantry.resources.view.ResourceItemView
 import com.herolynx.elepantry.resources.view.ResourceList
@@ -31,66 +18,20 @@ import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 
 
-class ResourcesActivity : AppCompatActivity() {
+class ResourcesActivity : LeftMenu() {
 
     private var googleDrive: GoogleDrive? = null
     private var googleSearch: GoogleDriveSearch? = null
     private var listAdapter: ListAdapter<Resource, ResourceItemView>? = null
 
-    private fun initViewHandlers() {
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener({ view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        })
-        initLeftMenuHandlers()
-    }
-
-    private fun initLeftMenuHandlers() {
-        val navigationView = findViewById(R.id.nav_view) as NavigationView
-        initUserViews(navigationView.menu)
-        getAppContext().map { c -> UserBadge(navigationView.getHeaderView(0) as ViewGroup).display(c.user!!) }
-        navigationView
-                .setNavigationItemSelectedListener { item ->
-                    debug("[LeftMenu] Item selected: %s", item.title)
-                    true
-                }
-    }
-
-    private fun initToolbar(toolbar: Toolbar) {
-        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
-        val toggle = ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer.setDrawerListener(toggle)
-        toggle.syncState()
-    }
+    override val layoutWithMenuId: Int = R.layout.resources_view
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.resources_view)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolbar)
-        initViewHandlers()
-        initToolbar(toolbar)
         initResourceView()
         googleDrive = GoogleDrive.create(this).get()
         googleSearch = googleDrive?.search()
         loadNextResults()
-    }
-
-    private fun initUserViews(menu: Menu) {
-        debug("[initUserViews] Creating...")
-        FirebaseDb.userViews.read()
-                .subscribe { uv ->
-                    var i = Menu.FIRST
-                    menu.clear()
-                    menu.add(0, i++, Menu.NONE, getString(R.string.google_drive)).setIcon(R.drawable.ic_menu_gallery)
-                    val menuUserViews = menu.addSubMenu(0, i++, Menu.NONE, getString(R.string.user_views))
-                    uv.views.map { v ->
-                        debug("[initUserViews] Adding view: %s", v.name)
-                        menuUserViews.add(i, i++, Menu.NONE, v.name).setIcon(R.drawable.ic_menu_share)
-                    }
-                }
     }
 
     private fun initResourceView() {
@@ -118,31 +59,6 @@ class ResourcesActivity : AppCompatActivity() {
                     listAdapter?.notifyDataSetChanged()
                     googleSearch = result
                 }
-    }
-
-    override fun onBackPressed() {
-        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.top_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        debug("[TopMenu] Item selected: %s", item.title)
-        val id = item.itemId
-
-        if (id == R.id.action_settings) {
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
     }
 
 }
