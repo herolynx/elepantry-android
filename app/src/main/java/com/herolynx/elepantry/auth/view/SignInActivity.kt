6f27749be63cadc8cf7ce375344a3838ced0e3fb
wrona <michal.robert.wronski@gmail.com>
2017-03-01 20:58:30 +0100

    [#9] Sign in ctrl added.

--- app/src/main/java/com/herolynx/elepantry/auth/view/SignInActivity.kt
+++ app/src/main/java/com/herolynx/elepantry/auth/view/SignInActivity.kt
@@ -5,80 +5,29 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 import android.widget.Button
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.common.api.GoogleApiClient
-import com.google.firebase.auth.FirebaseUser
-import com.herolynx.elepantry.Intents
 import com.herolynx.elepantry.R
-import com.herolynx.elepantry.auth.SignInUseCase
-import com.herolynx.elepantry.core.log.debug
-import com.herolynx.elepantry.core.log.error
-import com.herolynx.elepantry.core.rx.observe
-import com.herolynx.elepantry.core.rx.schedule
-import com.herolynx.elepantry.core.ui.navigation.navigateTo
 import com.herolynx.elepantry.core.ui.notification.WithProgressDialog
-import com.herolynx.elepantry.core.ui.notification.toast
-import com.herolynx.elepantry.ext.google.GoogleApi
-import com.herolynx.elepantry.getAuthContext
-import com.herolynx.elepantry.resources.view.ResourcesActivity
-import rx.Observable
 
 class SignInActivity : FragmentActivity(), WithProgressDialog {
 
     override var mProgressDialog: ProgressDialog? = null
-
-    private var api: GoogleApiClient? = null
+    private var ctrl: SignInCtrl? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_in_view)
-        api = GoogleApi.build(this)
-        initActionHandlers(api!!)
-        handleAuthResults(SignInUseCase.autoLogIn(api!!))
+        ctrl = SignInCtrl(this)
+        initActionHandlers(ctrl!!)
+        ctrl?.autoLogIn()
     }
 
-    private fun handleAuthResults(o: Observable<Pair<GoogleSignInAccount, FirebaseUser?>>) {
-        o.schedule()
-                .observe()
-                .subscribe(
-                        { auth -> onAuthResult(auth) },
-                        { ex -> onAuthError(ex) }
-                )
-    }
-
-    private fun initActionHandlers(api: GoogleApiClient) {
-        val signIn = findViewById(R.id.sign_in_button) as Button
-        signIn.setOnClickListener {
-            showProgressDialog(this)
-            SignInUseCase.startLogIn(this, api)
-        }
+    private fun initActionHandlers(ctrl: SignInCtrl) {
+        val buttonSignIn = findViewById(R.id.sign_in_button) as Button
+        buttonSignIn.setOnClickListener { ctrl.logIn() }
     }
 
     public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == Intents.GOOGLE_SIGN_IN) {
-            handleAuthResults(SignInUseCase.onLoginResult(data))
-        }
+        ctrl?.handleLoginResult(requestCode, data)
     }
-
-    private fun onAuthError(t: Throwable, showToast: Boolean = false) {
-        hideProgressDialog()
-        error("[Auth] Couldn't log in user", t)
-        if (showToast) {
-            toast(R.string.auth_failed, "Sign up failed")
-        }
-    }
-
-    private fun onAuthResult(auth: Pair<GoogleSignInAccount, FirebaseUser?>) {
-        debug("[Auth] User is logged in - auth: $auth, api connected: ${api?.isConnected}")
-        hideProgressDialog()
-        api?.disconnect()
-        if (auth.second != null) {
-            getAuthContext().map { c -> c.setMainAccount(auth.first) }
-            navigateTo(ResourcesActivity::class.java)
-        } else {
-            toast(R.string.auth_failed, "Sign up failed")
-        }
-    }
-
 }
