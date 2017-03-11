739f93862907b2bd647d61c72ab00aa432ba5cbe
wrona <michal.robert.wronski@gmail.com>
2017-03-11 10:44:28 +0100

    [#43] Displaying of resource tags and ext added.

--- app/src/main/java/com/herolynx/elepantry/resources/view/ui/ResourceList.kt
+++ app/src/main/java/com/herolynx/elepantry/resources/view/ui/ResourceList.kt
@@ -5,19 +5,32 @@ import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.herolynx.elepantry.R
+import com.herolynx.elepantry.config.Config
+import com.herolynx.elepantry.core.repository.Repository
 import com.herolynx.elepantry.core.ui.recyclerview.ListAdapter
 import com.herolynx.elepantry.resources.model.Resource
+import org.funktionale.option.Option
+import org.funktionale.option.toOption
 
 object ResourceList {
 
-    fun adapter(): ListAdapter<Resource, ResourceItemView> =
+    fun adapter(userResourceRepository: Repository<Resource> = Config.repository.userResources()):
+            ListAdapter<Resource, ResourceItemView> =
             ListAdapter(
                     { ctx -> ResourceItemView(ctx) },
-                    { r, h -> display(r, h) }
+                    { r, h -> display(r, h, userResourceRepository) }
             )
 
-    fun display(r: Resource?, h: ListAdapter.ViewHolder<ResourceItemView>) {
+    fun display(r: Resource?, h: ListAdapter.ViewHolder<ResourceItemView>, userResourceRepository: Repository<Resource>) {
         h.view.name.text = r?.name
+        h.view.ext.text = r?.extension
+        r.toOption()
+                .map(Resource::id)
+                .flatMap { id -> userResourceRepository.find(id).getOrElse { Option.None } }
+                .map { userResource ->
+                    h.view.ext.text = userResource.extension
+                    h.view.tags.text = userResource.tags.map { t -> "#${t.name}" }.reduce { t, s -> "$t, $s" }
+                }
     }
 
 }
@@ -29,6 +42,8 @@ class ResourceItemView(ctx: Context) : LinearLayout(ctx) {
     }
 
     val name = findViewById(R.id.resource_item_name) as TextView
+    val tags = findViewById(R.id.resource_item_tags) as TextView
+    val ext = findViewById(R.id.resource_item_ext) as TextView
 
 }
 
