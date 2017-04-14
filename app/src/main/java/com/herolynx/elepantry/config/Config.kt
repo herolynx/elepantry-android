3e932c533c5ca2cf0680c3011a8116fd07673941
wrona <michal.robert.wronski@gmail.com>
2017-04-14 08:30:32 +0200

    Links to pages added.

--- app/src/main/java/com/herolynx/elepantry/config/Config.kt
+++ app/src/main/java/com/herolynx/elepantry/config/Config.kt
@@ -7,4 +7,8 @@ object Config {
     //TODO load it from config
     val licenseUrl = "https://github.com/herolynx/elepantry-android/blob/master/LICENSE"
 
+    val elepantryUrl = "http://elepantry.com"
+
+    val herolynxUrl = "http://herolynx.com"
+
 }
